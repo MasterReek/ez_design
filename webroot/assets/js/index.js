0435dd97280bd00f0a86d70444704e6fa9512e8a
Takashi <reek@mac.com>
2019-06-11 16:17:01 +0900

    作業開始

--- webroot/assets/js/index.js
+++ webroot/assets/js/index.js
@@ -48,6 +48,7 @@ $(document).ready( function() {
     }
 
     telkiller();
+    isPhone();
     
 });
 
@@ -119,3 +120,32 @@ e.preventDefault();
 }
 
  }
+
+ $(function()
+{
+	if(!isPhone())
+	{
+		return;
+	}
+	$('span[data-action=call]').each(function()
+	{
+		var $ele = $(this);
+		$ele.wrap('<a href="tel:' + $ele.data('tel') + '"></a>');
+	});
+});
+
+function isPhone()
+{
+	// Edgeを弾く
+	if(navigator.userAgent.indexOf('Edge') >= 0)
+	{
+		return false;
+	}
+	// Android且つMobileだった場合、電話機とみなす
+	if(navigator.userAgent.indexOf('Android') >= 0 && navigator.userAgent.indexOf('Mobile') >= 0)
+	{
+		return true;
+	}
+	// 最後はiPhoneかどうかを判定し、結果を返す
+	return(navigator.userAgent.indexOf('iPhone') >= 0);
+}
